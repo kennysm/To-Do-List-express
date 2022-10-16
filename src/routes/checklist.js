b95c9b8ef3e0cd7abddbe8b08b790aa54bb61976
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-10-16 17:57:15 -0300

    att

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,19 +2,34 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  console.log("Olá");
-  res.send();
+const Checklist = require("../models/checklist");
+
+router.get("/", async (req, res) => {
+  try {
+    let checklists = await Checklist.find({});
+    res.status(200).json(checklists);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.status(200).send(req.body);
+router.post("/", async (req, res) => {
+  let { name } = req.body;
+  try {
+    let checklist = await Checklist.create({ name });
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
 });
 
-router.get("/:id", (req, res) => {
-  console.log(req.body);
-  res.send(`ID: ${req.params.id}`);
+router.get("/:id", async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id);
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
 });
 
 router.put("/:id", (req, res) => {
