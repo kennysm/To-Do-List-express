ceb1a55edb9893ece5f41bb0965c4a0b5e8a0d73
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-10-17 12:34:35 -0300

    sending data to a form

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -16,13 +16,28 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
-  let { name } = req.body;
+router.get("/new", async (req, res) => {
   try {
-    let checklist = await Checklist.create({ name });
-    res.status(200).json(checklist);
+    let checklist = new Checklist();
+    res.status(200).render("checklists/new", { checklist: checklist });
   } catch (error) {
-    res.status(422).json(error);
+    res
+      .status(500)
+      .render("pages/error", { errors: "Erro ao carregar o formulário" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  let { name } = req.body.checklist;
+  let checklist = new Checklist({ name });
+
+  try {
+    await checklist.save();
+    res.redirect("/checklists");
+  } catch (error) {
+    res
+      .status(422)
+      .render("checklists/new", { checklists: { ...checklist, error } });
   }
 });
 
@@ -32,7 +47,7 @@ router.get("/:id", async (req, res) => {
     res.status(200).render("checklists/show", { checklist: checklist });
   } catch (error) {
     res
-      .status(200)
+      .status(500)
       .render("pages/error", { error: "erro ao exibir as listas de tarefas" });
   }
 });
