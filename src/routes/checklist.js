e9fd00da8ccfd57018df4d2d2ce8fe2430d28b7f
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-10-16 22:56:56 -0300

    att to see a task

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const checklist = require("../models/checklist");
 
 const router = express.Router();
 
@@ -7,9 +8,11 @@ const Checklist = require("../models/checklist");
 router.get("/", async (req, res) => {
   try {
     let checklists = await Checklist.find({});
-    res.status(200).json(checklists);
+    res.status(200).render("checklists/index", { checklists: checklists });
   } catch (error) {
-    res.status(500).json(error);
+    res
+      .status(200)
+      .render("pages/error", { error: "erro ao exibir as listas" });
   }
 });
 
@@ -26,9 +29,11 @@ router.post("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     let checklist = await Checklist.findById(req.params.id);
-    res.status(200).json(checklist);
+    res.status(200).render("checklists/show", { checklist: checklist });
   } catch (error) {
-    res.status(422).json(error);
+    res
+      .status(200)
+      .render("pages/error", { error: "erro ao exibir as listas de tarefas" });
   }
 });
 
